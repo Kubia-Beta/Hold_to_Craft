c22822db1f60985a1e4f1f447f0095295dc389ea
Kubia-Beta <113896543+Kubia-Beta@users.noreply.github.com>
2023-02-28 21:13:38 -0700

    Update and rename Keybinding.cpp to htc.cpp
    Multiple cout changes, some ini creation changes. Some comments removed.
    Does not have custom keybind functionality.

--- htc.cpp
+++ htc.cpp
@@ -31,11 +31,11 @@ int InputValidation() {
 		if (!std::cin) { //hello failbit, I see someone tried to put a non int again
 			std::cin.clear();
 			std::cin.ignore();
-			std::cerr << "\nInvalid input. Enter input: ";
+			std::cerr << "\nInvalid input. ";
 		} else if (input == 0 || input == 1) {
 			return input;
 		} else {
-			std::cerr << "\nInvalid input. Enter input: ";
+			std::cerr << "\nInvalid input. ";
 		}
 	}
 }
@@ -53,8 +53,9 @@ void Keybinding() { //one time setup, called only if .\lib\htcprefs.ini does not
 	int navigator = 0; //initialize at 0 for safety.
 	std::cout << "Use the Default keybinds [1] or Custom keybinds [0]?\n";
 	navigator = InputValidation();
-	if (navigator == 0) { //TODO: custom keybinds
-		outfile << "Custom inputs not yet implemented.\n[controls]\npause=F1\nexit=F4";
+	if (navigator == 0) { //TODO: custom keybinds setup
+		std::cout << "\n-----This test build does not have custom keybinds-----\n-----Default keybinds used-----\n";
+		outfile << "[error]\nerrorlevel=1\n[controls]\npause=F1\nexit=F4";
 		pressAnyKeyToContinue();
 	}
 	else if (navigator == 1) {
@@ -63,55 +64,27 @@ void Keybinding() { //one time setup, called only if .\lib\htcprefs.ini does not
 	}
 	else {
 		std::cerr << "\n\nSomething went wrong with the one time setup.\nAll defaults assumed.\nCustom controls can be manually entered into \\lib\\htcprefs.ini.\n";
-		outfile << "Unexpected error on file creation process. Defaults assumed.\n[controls]\npause=F1\nexit=F4\n[sound]\nnoise=0";
+		outfile << "[error]\nerrorlevel=2[controls]\npause=F1\nexit=F4\n[sound]\nnoise=0";
 		return;
 	}
 	navigator = 0; //reset
 	std::cout << "Have the sound beeps OFF [1] or have them ON [0]?\n";
 	navigator = InputValidation();
-	if (navigator == 0) { //TODO: custom keybinds
-		outfile << "\n[sound]\nnoise=0";
+	if (navigator == 0) {
+		outfile << "\n[sound]\nnoise=1";
 	}
 	else if (navigator == 1) {
-		outfile << "\n[sound]\nnoise=1";
+		outfile << "\n[sound]\nnoise=0";
 	}
 	else {
 		std::cerr << "\n\nSomething went wrong with the one time setup.\nSound defaulted of off (0).\nSound option can be manually entered into \\lib\\htcprefs.ini.\n";
-		outfile << "\n[sound]\nnoise=0";
+		outfile << "\n[sound]\nnoise=0\n[error]\nerrorlevel=3";
 		return;
 	}
 
-
-	//[sound]\nnoise=0
 	outfile.close(); //always close the file at the end of one time setup
 	std::cout << "One time setup finished. You can always modify the settings in \\lib\\htcprefs.ini.\n";
 	pressAnyKeyToContinue();
-	/*	open file as ini
-			"\ndefault keybinds [0] or one button custom keybinds [1] ?"
-			if 1{
-			cin >> pauseKey
-			cout << "Pause keybind set to: " << pausekey << endl;
-
-			cin >> exitKey
-			"Sound beeps off [0] or on [1]?"
-
-			cin >> noiseVal
-			} else {
-			cout << "Using default keys F1 to pause and F4 to exit with sound OFF";
-			pauseKey = "F1"
-			exitKey = "F4"
-			noiseVal = "0"; //1 is on 0 is off
-
-
-			write to file:
-			"[controls]"
-			"pause = "pauseKey
-			"exit = "exitKey
-
-			"[sound]"
-			"noise = "noiseVal
-
-			close file */
 }
 
 int main() {
@@ -119,14 +92,15 @@ int main() {
 	std::string filePath = ".\\lib\\htcprefs.ini";
 	if (!std::filesystem::exists(filePath)) { //if it DOESNT exist
 		std::cout << "Now running one time setup.\nAfter inputting your option, press [Enter] to continue.\nOptions will be displayed in braces, so option 0 is [0].\n";
+		pressAnyKeyToContinue();
 		Keybinding();
 	} else {
-		std::cout << "htcprefs.ini detected.\n";
+		std::cout << "htcprefs.ini detected.\nYou can always modify the settings in \\lib\\htcprefs.ini.\n";
 	}
 
 
 	std::cout << "\nNow running 'Hold to Craft' for No Man's Sky.\nThis terminal will exit when 'Hold to Craft' has exited.\nClosing this terminal will not terminate 'Hold to Craft'.\n";
-	system(".\\lib\\secondTest.ahk"); //launches the AHK responsible for the game interaction
+	system(".\\lib\\htc.exe"); //launches the AHK responsible for the game interaction
 	std::cout << "\n\n'Hold to Craft' exited. Now closing host program...";
 	std::this_thread::sleep_for(std::chrono::milliseconds(5000)); //user can see the closing message for 5 seconds.
 	return 0;
